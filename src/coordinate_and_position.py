c2502111fb7e8ce299f4aa706b19f3d455104a2c
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2023-04-17 21:23:43 -0400

    from type_extensions import Alias

--- src/coordinate_and_position.py
+++ src/coordinate_and_position.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from typing import Tuple, TypeAlias, Union
+from typing import Tuple, Union
+from typing_extensions import TypeAlias
 
 
 Position: TypeAlias = Tuple[Union[int, float], Union[int, float]]
