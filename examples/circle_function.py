cfb917517fd534e58b2568b10fb09bb1b60086d8
Benjamin Lacasse <belac95@ulaval.ca>
2023-05-12 11:43:54 -0400

    d cartesien done

--- examples/circle_function.py
+++ examples/circle_function.py
@@ -55,15 +55,15 @@ def top_circle_function2(precision, tuple_1, tuple_2):
     
     return posi_dict
 
-def straight_lines(precision, tuple_1, tuple_2):
-    delta_x = (tuple_2[0] - tuple_1[0]) / precision
-    delta_y = (tuple_2[1] - tuple_1[1]) / precision
-    posi_dict = {}
-    posi_list = []
-    for i in range(precision+1):
-        posi_dict[i] = (tuple_1[0] + delta_x * i, tuple_1[1] + delta_y * i)
+# def straight_lines(precision, tuple_1, tuple_2):
+#     delta_x = (tuple_2[0] - tuple_1[0]) / precision
+#     delta_y = (tuple_2[1] - tuple_1[1]) / precision
+#     posi_dict = {}
+#     posi_list = []
+#     for i in range(precision+1):
+#         posi_dict[i] = (tuple_1[0] + delta_x * i, tuple_1[1] + delta_y * i)
     
-    return posi_dict
+#     return posi_dict
       
 #print(straight_lines(10, (10,0), (0, 10)))
 
@@ -83,4 +83,5 @@ def full_arch(precision, tuple_t1, tuple_t2, tuple_b1, tuple_b2):
 # print(top_circle_function2(4, (12/13 * 70, 5/13 * 70), (5/13 * 70, 12/13 * 70)))
 # print("------------------------------------------")
 # print(full_arch(4, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100), (5/13 * 70, 12/13 * 70), (12/13 * 70, 5/13 * 70)))
-    
+# for value in full_arch(4, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100), (5/13 * 70, 12/13 * 70), (12/13 * 70, 5/13 * 70)).values():
+#     print(value)
--- examples/circuit d-test.py
+++ examples/circuit d-test.py
@@ -5,14 +5,22 @@ from math import sqrt
 
 from src import Circuit, CoordinateSystem, VoltageSource, Wire, World
 
-from circle_function import top_circle_function2, straight_lines, full_arch
+from circle_function import full_arch
 
 if __name__ == "__main__":
     WORLD_SHAPE = (101, 101)
-    precision = 10
-    BATTERY_VOLTAGE = 1.0/precision
-    HIGH_WIRE_RESISTANCE = 1.0/precision
-    LOW_WIRE_RESISTANCE = 0.01/precision
+    
+    #longueur de la batterie ainsi que son voltage adapté pour garder un voltage de 1 V total
+    Battery_length = 1
+    BATTERY_VOLTAGE = 1.0 / Battery_length
+    
+    #longueur de la haute résistance ainsi que sa resistance adapté pour garder 1 ohm
+    High_resistance_length = 2
+    HIGH_WIRE_RESISTANCE = 1.0/High_resistance_length
+    
+    #nombre de segments pour représenter les deux arcs de cercles
+    precision = 5
+    LOW_WIRE_RESISTANCE = 0.01/(2*precision + 2 - Battery_length - High_resistance_length)
 
     cartesian_variables = Symbol("x"), Symbol("y")
     x, y = cartesian_variables
@@ -23,45 +31,52 @@ if __name__ == "__main__":
     diagonal_eqs = (x_expression_diagonal, y_expression_diagonal)
 
     wires = []
-    
-    #Low resistance big radius
-    for k in range(precision):
-        wires.append(Wire(top_circle_function2(precision, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100))[k], top_circle_function2(precision, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100))[k+1], 
+    #on définit un dictionaire qui contient tous les points du circuit
+    arch = full_arch(precision, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100), (5/13 * 70, 12/13 * 70), (12/13 * 70, 5/13 * 70))
+
+    #top gauche à haute résistance
+    for key in range(int((precision - High_resistance_length)/2)):
+        # print('lr', key)
+        # print((arch[key], arch[key +1]))
+        wires.append(Wire(arch[key], arch[key+1],
                           diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
-        # wires.append((top_circle_function2(precision, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100))[k], top_circle_function2(precision, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100))[k+1]))
     
-    #Low resistance right line
-    # for k in range(precision):
-    #     wires.append(Wire(straight_lines(precision, (12/13 * 100, 5/13 * 100), (12/13 * 70, 5/13 * 70))[k], straight_lines(precision, (12/13 * 100, 5/13 * 100), (12/13 * 70, 5/13 * 70))[k+1], 
-    #                       diagonal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE))
-    
-    wires.append(Wire((12/13 * 100, 5/13 * 100), (12/13 * 70, 5/13 * 70),
-                      diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
-    #     wires.append(((12/13 * 100, 5/13 * 100), (12/13 * 70, 5/13 * 70)))
+    #haute resistance    
+    for key in range(int((precision - High_resistance_length)/2), int((precision + High_resistance_length)/2)):
+        # print('hr', key)
+        # print((arch[key], arch[key +1]))
+        wires.append(Wire(arch[key], arch[key+1],
+                          diagonal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE))
         
-    #Low resistance small radius
-    for k in range(precision):
-        wires.append(Wire(top_circle_function2(precision, (12/13 * 70, 5/13 * 70), (5/13 * 70, 12/13 * 70))[k], top_circle_function2(precision, (12/13 * 70, 5/13 * 70), (5/13 * 70, 12/13 * 70))[k+1], 
+    #haute resistance à batterie
+    for key in range(int((precision + High_resistance_length)/2), int(3/2*precision + 1 - Battery_length/2)):
+        # print('lr', key)
+        # print((arch[key], arch[key +1]))
+        wires.append(Wire(arch[key], arch[key+1],
                           diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
-        # wires.append((top_circle_function2(precision, (12/13 * 70, 5/13 * 70), (5/13 * 70, 12/13 * 70))[k], top_circle_function2(precision, (12/13 * 70, 5/13 * 70), (5/13 * 70, 12/13 * 70))[k+1]))
+        
+    #batterie
+    for key in range(int(3/2*precision + 1 - Battery_length/2), int(3/2*precision + 1 + Battery_length/2)):
+        # print('B', key)
+        # print((arch[key], arch[key +1]))
+        wires.append(VoltageSource(arch[key], arch[key+1],
+                          diagonal_eqs, cartesian_variables, BATTERY_VOLTAGE))
+        
+    #batterie à top gauche
+    for key in range(int(3/2*precision + 1 + Battery_length/2), int(2*precision+2)):
+        # print('lr', key)
+        # print((arch[key], arch[key +1]))
+        wires.append(Wire(arch[key], arch[key+1],
+                          diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
     
-    #Low resistance left line
-    # for k in range(precision):
-    #     wires.append(Wire(straight_lines(precision, (5/13 * 70, 12/13 * 70), (5/13 * 100, 12/13 * 100))[k], straight_lines(precision, (5/13 * 70, 12/13 * 70), (5/13 * 100, 12/13 * 100))[k+1], 
-    #                       diagonal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE))
+    #on définit la ground_position comme étant juste avant la batterie
+    ground_position = (arch[int(3/2*precision + 1 - Battery_length/2)])
     
-    wires.append(Wire((5/13 * 70, 12/13 * 70), (5/13 * 100, 12/13 * 100), 
-                      diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
-    #     wires.append(((5/13 * 70, 12/13 * 70), (5/13 * 100, 12/13 * 100)))
     
-    # for k in wires:
-    #     print(k)   
-    
-    ground_position = (5/13 * 70, 12/13 * 70)
     circuit = Circuit(wires, ground_position)
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
     world.show_circuit(
-        full_arch(precision, (5/13 * 100, 12/13 * 100), (12/13 * 100, 5/13 * 100), (5/13 * 70, 12/13 * 70), (12/13 * 70, 5/13 * 70))
+        arch
     )
     world.compute()
     world.show_all()
