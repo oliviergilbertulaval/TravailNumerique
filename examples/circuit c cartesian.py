d79ba9b2c3d973bb5fbd092327d19e7f829d4390
Benjamin Lacasse <belac95@ulaval.ca>
2023-05-14 15:43:13 -0400

    circuit c réparé

--- examples/circuit c cartesian.py
+++ examples/circuit c cartesian.py
@@ -10,7 +10,7 @@ from circle_function import top_circle_function, bottom_circle_function, full_ci
 if __name__ == "__main__":
     WORLD_SHAPE = (101, 101)
     precision = 30
-    BATTERY_VOLTAGE = -1.0/precision
+    BATTERY_VOLTAGE = 1.0/precision
     HIGH_WIRE_RESISTANCE = 1.0/precision
     LOW_WIRE_RESISTANCE = 0.01/precision
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         wires.append(Wire(top_circle_function(precision, (35, 70), (65, 70))[k], top_circle_function(precision, (35, 70), (65, 70))[k+1], 
                           diagonal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE))
         
-    #Low resistance wire from (65, 70) to (65, 10)
+    #Low resistance wire from (65, 70) to (65, 30)
     for k in range(precision):
         wires.append(Wire(top_circle_function(precision, (65, 70), (75, 50))[k], top_circle_function(precision, (65, 70), (75, 50))[k+1], 
                           diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
@@ -37,12 +37,12 @@ if __name__ == "__main__":
         wires.append(Wire(bottom_circle_function(precision, (75, 50), (65, 30))[k], bottom_circle_function(precision, (75, 50), (65, 30))[k+1], 
                           diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
 
-    #Voltage from (65, 10) to (35, 10)
+    #Voltage from (65, 30) to (35, 30)
     for k in range(precision):
         wires.append(VoltageSource(bottom_circle_function(precision, (65, 30), (35, 30))[k], bottom_circle_function(precision, (65, 30), (35, 30))[k+1], 
                           diagonal_eqs, cartesian_variables, BATTERY_VOLTAGE))
         
-    #Low resistance wire from (35, 10) to (35, 70)
+    #Low resistance wire from (35, 30) to (35, 70)
     for k in range(precision):
         wires.append(Wire(bottom_circle_function(precision, (35, 30), (25, 50))[k], bottom_circle_function(precision, (35, 30), (25, 50))[k+1], 
                           diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
@@ -50,7 +50,7 @@ if __name__ == "__main__":
         wires.append(Wire(top_circle_function(precision, (25, 50), (35, 70))[k], top_circle_function(precision, (25, 50), (35, 70))[k+1], 
                           diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE))
 
-    ground_position = (25, 50)
+    ground_position = (65, 30)
     circuit = Circuit(wires, ground_position)
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
     world.show_circuit(
